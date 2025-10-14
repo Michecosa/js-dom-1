623f7f3470301f51ebe5e91bc5b34e29de2b4fa2
Michela <micheladellagatta1@gmail.com>
2025-10-14 15:17:59 +0200

    Write code default version

--- bonus/assets/js/script.js
+++ bonus/assets/js/script.js
@@ -11,15 +11,10 @@ Per fare questo bonus potremmo aver bisogno di del metodo string.includes()
 */
 
 const lampadina = document.getElementById('pic');
-const srcLampOn = './assets/img/yellow_lamp.png';
-const srcLampOff = './assets/img/white_lamp.png';
 const bottone = document.querySelector('#bottone');
 
-bottone.addEventListener('click', function turnOnOff() {
-  const getLampId = lampadina.getAttribute('src');
-  if (getLampId.includes('white_lamp.png')) {
-    lampadina.src = srcLampOn;
-  } else {
-    lampadina.src = srcLampOff;
+bottone.addEventListener('click',
+  function turnOn() {
+    lampadina.src = './assets/img/yellow_lamp.png';
   }
-});
+)
