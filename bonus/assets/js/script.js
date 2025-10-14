528ee7be6598fe030a57b35e8f182ee30f8f923c
Michela <micheladellagatta1@gmail.com>
2025-10-14 14:19:50 +0200

    Add new folder for bonus

---
+++ bonus/assets/js/script.js
@@ -0,0 +1,23 @@
+/* 
+Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+
+Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+
+BONUS
+
+Facciamo accendi e spegni:
+
+Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+E così via...
+
+Per fare questo bonus potremmo aver bisogno di del metodo string.includes()
+*/
+
+const lampadina = document.getElementById('pic');
+
+lampadina.addEventListener('click',
+  function turnOn() {
+    lampadina.src = './assets/img/yellow_lamp.png';
+  }
+)
